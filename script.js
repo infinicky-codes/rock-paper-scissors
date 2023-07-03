c8ddb761e3099f1fb199e482ddbeaa45c0a58542
infinicky <infinicky.codes@gmail.com>
2023-07-03 12:11:22 +0200

    Feat: Display results to screen

--- script.js
+++ script.js
@@ -3,58 +3,85 @@ let playerScore = 0;
 // Keep track of the computer's score
 let computerScore = 0;
 
-// Wait for the player to choose something
+// Keep track of the round number
+let round = 1;
+
+const container = document.querySelector('.content');    
+
+
+// Let player choose something
 getSelections();
-
-// Function that plays 5 rounds of Rock Paper Scissors
-function game() {
-    // keep track of the round number
-    let round = 1;
-
-    // play 5 rounds
-    // while (round <= 5) {        
-    //     // get the computer's choice
-    //     let computerSelection = getComputerSelection();
-    //     // play a round and log the result
-    //     console.log(playRound(playerSelection, computerSelection));
-    //     round++;
-    // }
-
-    // determine the winner
-    console.log("Final round played. Determining the winner...");
-    if (playerScore > computerScore) {
-        console.log("You win! You beat the computer by " + (playerScore - computerScore) + " points.");
-    }
-    else if (playerScore < computerScore) {
-        console.log("You lose by " + (computerScore - playerScore) + " points.");
-        console.log("Better luck next time!");
-    }
-    else {
-        console.log("It's a tie! You both have " + playerScore + " points.");
-    }
-}
 
 // Get player and Computer selections 
 function getSelections() {
-    // // get the player's selection
+    // Get the player's selection
     const selections = document.querySelectorAll('button');
 
     selections.forEach(selection => {
         selection.addEventListener('click', () => 
             console.log(playRound(selection.id, getComputerSelection())));
+            //playRound(selection.id, getComputerSelection()));
     });
 }
+
+// Display intermediate result
+function showRoundResult(result) {
+    let resultMessage = document.createElement('h2');
+    resultMessage.textContent = result;
+    container.appendChild(resultMessage);
+}
+
+// Display end of game
+function displayEndOfGame() {
+    let determinationMessage = document.createElement('h2');
+    determinationMessage.textContent = "Final round played. Determining the winner...";
+    container.appendChild(determinationMessage);
+}
+
+// Determine final result
+function determineFinalResult() {
+    let finalMessage = document.createElement('h2');
+    
+    if (playerScore > computerScore) {
+        finalMessage.textContent = "You win! You beat the computer by " + 
+        (playerScore - computerScore) + " points.";
+        finalMessage.style.color = 'green';
+    }
+    else if (playerScore < computerScore) {
+        finalMessage.textContent = "You lose by " + (computerScore - playerScore) + 
+        " points.";
+        finalMessage.textContent += "\n Better luck next time!";
+        finalMessage.style.color = 'red';
+    }
+    else {
+        finalMessage.textContent = "It's a tie! You both have " + playerScore + " points.";
+    }
+    
+    container.appendChild(finalMessage);
+}
+
+
 
 // Single round of Rock Paper Scissors
 function playRound(playerSelection, computerSelection) {
     // Match playerSelection and computerSelection cases
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);    // computerSelection = computerSelection.toLowerCase();
+    playerSelection = playerSelection.charAt(0).toUpperCase() + 
+                      playerSelection.slice(1);    
 
     let playerWins = false;
 
+    // End the game if this has been the 5th round
+    round++;
+    if (round === 6) {
+        displayEndOfGame();
+        determineFinalResult();
+    }
+
     // if the player and computer choose the same thing, it's a tie
     if (playerSelection === computerSelection) {
-        return "It's a tie!";
+        //container.removeChild(document.getElementsByTagName('h2'));
+        showRoundResult("It's a tie!");
+        return;
     }
     
     // rock beats scissors
@@ -90,11 +117,11 @@ function playRound(playerSelection, computerSelection) {
     // return the result of the round
     if (playerWins) {
         playerScore++;
-        return "You win! " + playerSelection + " beats " + computerSelection;
+        showRoundResult("You win! " + playerSelection + " beats " + computerSelection);
     }
     else {
         computerScore++;
-        return "You lose! " + computerSelection + " beats " + playerSelection;
+        showRoundResult("You lose! " + computerSelection + " beats " + playerSelection);
     }
 }
 
